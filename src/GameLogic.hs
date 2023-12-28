3c84f521c5a46a854e61fe284f1688dd0423c646
Kelly Brower <kellybrower@Kellys-MacBook-Pro.local>
2023-12-28 10:45:11 -0500

    Added safe indexing and Maybe Player

--- src/GameLogic.hs
+++ src/GameLogic.hs
@@ -13,10 +13,11 @@ initGame :: Int -> Game
 initGame numPlayers = replicate numPlayers (Player 1 3)
 
 -- Roll the dice for a player
-rollDice :: Player -> IO [DiceOutcome]
-rollDice player = mapM (const rollOne) [1 .. numDice]
+rollDice :: Maybe Player -> IO [DiceOutcome]
+rollDice Nothing = return [] -- If there is no player, return an empty list of outcomes.
+rollDice (Just player) = mapM (const rollOne) [1 .. numDice]
   where
-    numDice = min 3 (chips player) -- Players roll up to 3 dice based on their chip count
+    numDice = min 3 (chips player) -- Players roll up to 3 dice based on their chip count.
 
     -- Roll a single die and get the outcome
     rollOne :: IO DiceOutcome
@@ -36,13 +37,22 @@ isGameOver game =
     hasChips :: Player -> Bool
     hasChips player = chips player > 0
 
+safeGet :: Int -> [a] -> Maybe a
+safeGet i xs
+  | i >= 0 && i < length xs = Just (xs !! i)
+  | otherwise = Nothing
+
 -- Assuming Game is a list of Players
 playRound :: Game -> Int -> IO Game
 playRound game playerNumber = do
-  let player = game !! (playerNumber - 1) -- Get the current player based on playerNumber
-  outcomes <- rollDice player
-  putStrLn $ "Player " ++ show (playerId player) ++ " rolled: " ++ show outcomes
-  let updatedGame = applyRules game outcomes playerNumber
+  let maybePlayer = safeGet (playerNumber - 1) game -- Get the current player based on playerNumber
+  outcomes <- rollDice maybePlayer
+  case maybePlayer of
+    Just player -> putStrLn $ "Player " ++ show (playerId player) ++ " rolled: " ++ show outcomes
+    Nothing -> putStrLn $ "Invalid player number."
+  let updatedGame = case maybePlayer of
+        Just player -> applyRules game outcomes playerNumber
+        Nothing -> game -- If the player is invalid, return the game unchanged.
   return updatedGame
 
 -- Implement the applyRules function
@@ -60,8 +70,8 @@ moveChip :: Game -> Int -> Int -> Game
 moveChip game currentPlayerIndex direction =
   let totalPlayers = length game
       nextIndex = (currentPlayerIndex + direction + totalPlayers) `mod` totalPlayers
-      gameWithUpdatedCurrentPlayer = updateList game currentPlayerIndex (updateCurrentPlayer (game !! currentPlayerIndex))
-      updatedGame = updateList gameWithUpdatedCurrentPlayer nextIndex (updateNextPlayer (game !! nextIndex))
+      gameWithUpdatedCurrentPlayer = maybe game (updateList game currentPlayerIndex . updateCurrentPlayer) (safeGet currentPlayerIndex game)
+      updatedGame = maybe gameWithUpdatedCurrentPlayer (updateList gameWithUpdatedCurrentPlayer nextIndex . updateNextPlayer) (safeGet nextIndex game)
    in updatedGame
   where
     updateCurrentPlayer player = player {chips = chips player - 1}
@@ -74,10 +84,15 @@ moveChip game currentPlayerIndex direction =
 centerChip :: Game -> Int -> Game
 centerChip game playerNumber =
   let currentPlayerIndex = playerNumber - 1 -- Adjusting playerNumber to 0-based index
-      currentPlayer = game !! currentPlayerIndex
-      updatedCurrentPlayer = currentPlayer {chips = chips currentPlayer - 1}
-   in updateList game currentPlayerIndex updatedCurrentPlayer
+      maybePlayer = safeGet currentPlayerIndex game
+      updatedGame = maybe game (updateGame currentPlayerIndex) maybePlayer
+   in updatedGame
   where
+    updateGame :: Int -> Player -> Game
+    updateGame index player =
+      let updatedPlayer = player {chips = chips player - 1}
+       in updateList game index updatedPlayer
+
     updateList :: [Player] -> Int -> Player -> [Player]
     updateList xs index newElement =
       take index xs ++ [newElement] ++ drop (index + 1) xs
